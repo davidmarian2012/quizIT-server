0e76fab53046d2e360cc16dd22cc1f41e87fecd0
David <davidmarian2012@gmail.com>
2022-12-19 04:54:05 +0200

    User controller and routes

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { Database } from './dbConnection.js';
-import User from './schemas/userSchema.js';
-import mongoose from 'mongoose';
+
+import UserRoutes from './routes/userRoutes.js';
 
 const app = express();
 
@@ -14,25 +14,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 const db = new Database();
 db.connect();
 
-app.get('/hello', (req, res) => {
-    res.send('test');
+app.get('/', (req, res) => {
+    res.send('Server is running');
 })
 
-app.post('/register', (req, res) => {
-    const user = new User({
-        _id: mongoose.Types.ObjectId(),
-        email: 'test2',
-        username: 'test2',
-        password: 'test'
-    });
-    user.save().then(()=>{
-        res.send({message: `Added user ${user.username}`})
-        console.log(`Added user ${user.username}`)
-        res.end();
-    }).catch((error)=>{
-        console.log(error);
-    })
-})
+app.use('/user', UserRoutes);
 
 const port = 8080;
 app.listen(port, () => console.log(`Backend application listening at http://localhost:${port}`));
