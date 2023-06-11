a14809feb93ee467554b4d133722b0017903053c
David <davidmarian2012@gmail.com>
2023-06-11 14:47:29 +0300

    v0.1

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,136 +2,169 @@ import User from "../schemas/userSchema.js";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import HttpStatus from "../enums/HttpStatusEnum.js";
-import jsonwebtoken from 'jsonwebtoken';
-import logger from '../services/logger.js';
+import jsonwebtoken from "jsonwebtoken";
+import logger from "../services/logger.js";
 
 const UserController = {
-    create: (req, res) => {
-        const userInput = req.body;
-        userInput._id = mongoose.Types.ObjectId();
-        userInput.points = 0;
+  create: (req, res) => {
+    const userInput = req.body;
+    userInput._id = mongoose.Types.ObjectId();
+    userInput.points = 0;
 
-        const newUser = new User(userInput);
-        newUser.save().then(() => {
-            res.status(HttpStatus.Created).send({username: newUser.username ,success: true})
-        })
-        .catch(error=>{
-            res.status(HttpStatus.ServerError).json({message: error.message});
-        })
-    },
+    const newUser = new User(userInput);
+    newUser
+      .save()
+      .then(() => {
+        res
+          .status(HttpStatus.Created)
+          .send({ username: newUser.username, success: true });
+      })
+      .catch((error) => {
+        res.status(HttpStatus.ServerError).json({ message: error.message });
+      });
+  },
 
-    login: async (req, res) => {
-        try{
-            let user = await User.findOne({username: req.body.username});
-            if (!user) return res.status(HttpStatus.Unauthorized).send('Invalid username/password');
+  login: async (req, res) => {
+    try {
+      let user = await User.findOne({ username: req.body.username });
+      if (!user)
+        return res
+          .status(HttpStatus.Unauthorized)
+          .send("Invalid username/password");
 
-            const validPass = await bcrypt.compare(req.body.password, user.password);
-            if (!validPass) return res.status(HttpStatus.Unauthorized).send('Invalid username/password');
+      const validPass = await bcrypt.compare(req.body.password, user.password);
+      if (!validPass)
+        return res
+          .status(HttpStatus.Unauthorized)
+          .send("Invalid username/password");
 
-            let response = {
-                id: user._id,
-                username: user.username,
-            }
+      let response = {
+        id: user._id,
+        username: user.username,
+      };
 
-            const jwtKey = process.env.JWT_SECRET_KEY || 'secret_key'
-            const token = jsonwebtoken.sign(
-                {user: response},
-                jwtKey,
-                {algorithm: 'HS256'}
-            )
+      const jwtKey = process.env.JWT_SECRET_KEY || "secret_key";
+      const token = jsonwebtoken.sign({ user: response }, jwtKey, {
+        algorithm: "HS256",
+      });
 
-            return res.status(HttpStatus.Ok).json({token: token})
-        }
-        catch(error){
-            logger.error(error.message);
-            res.status(HttpStatus.ServerError).json({message: error.message});
-        }
-    },
+      return res.status(HttpStatus.Ok).json({ token: token });
+    } catch (error) {
+      logger.error(error.message);
+      res.status(HttpStatus.ServerError).json({ message: error.message });
+    }
+  },
 
-    getAll: async (req, res) => {
-        try{
-            let users = await User.find();
-            return res.status(HttpStatus.Ok).json(users);
-        } catch (error){
-            res.status(HttpStatus.ServerError).json({message: error.message});
-        }
-    },
+  resetPassword: async (req, res) => {
+    try {
+      let user_ = await User.findOne({ username: req.body.username });
+      if (!user_)
+        return res.status(HttpStatus.Unauthorized).send("Invalid username");
 
-    getAllByPoints: async (req, res) => {
-        try{
-            let users = await User.find().sort({points: 'desc'});
-            return res.status(HttpStatus.Ok).json(users);
-        } catch (error){
-            res.status(HttpStatus.ServerError).json({message: error.message});
-        }
-    },
+      const validPass = await bcrypt.compare(
+        req.body.oldpassword,
+        user_.password
+      );
+      if (!validPass) {
+        return res.status(HttpStatus.Unauthorized).send("Invalid password");
+      }
 
-    getUserByUsername: async (req, res) => {
-        try{
-            let user = await User.findOne({
-                username: req.body.username
-            });
+      user_.password = req.body.newpassword;
+      await user_.save();
+      return res.status(HttpStatus.Ok).send("Password changed successfully");
+    } catch (error) {
+      logger.error(error.message);
+      res.status(HttpStatus.ServerError).json({ message: error.message });
+    }
+  },
 
-            if(user){
-                res.status(HttpStatus.Ok).json(user);
-            }
-            res.status(HttpStatus.Ok).json();
-        } catch (error){
-            logger.error(error.message);
-            res.status(HttpStatus.ServerError).json({message: error.message});
-        }
-    },
+  getAll: async (req, res) => {
+    try {
+      let users = await User.find();
+      return res.status(HttpStatus.Ok).json(users);
+    } catch (error) {
+      res.status(HttpStatus.ServerError).json({ message: error.message });
+    }
+  },
 
-    upload: async (req, res) => {
-        try{
-            const user = await User.findOne({
-                username: req.body.username
-            });
+  getAllByPoints: async (req, res) => {
+    try {
+      let users = await User.find().sort({ points: "desc" });
+      return res.status(HttpStatus.Ok).json(users);
+    } catch (error) {
+      res.status(HttpStatus.ServerError).json({ message: error.message });
+    }
+  },
 
-            if(user){
-                user.avatar = req.file.filename;
-                await user.save();
-                res.status(HttpStatus.Ok).json({message: 'Avatar uploaded successfully!'});
-            } 
+  getUserByUsername: async (req, res) => {
+    try {
+      let user = await User.findOne({
+        username: req.body.username,
+      });
 
-        } catch (error){
-            res.status(HttpStatus.ServerError).json({message: error.message});
-        }
-    },
+      if (user) {
+        res.status(HttpStatus.Ok).json(user);
+      }
+      res.status(HttpStatus.Ok).json();
+    } catch (error) {
+      logger.error(error.message);
+      res.status(HttpStatus.ServerError).json({ message: error.message });
+    }
+  },
 
-    removeAvatar: async (req, res) => {
-        try{
-            const user = await User.findOne({
-                username: req.body.username
-            });
+  upload: async (req, res) => {
+    try {
+      const user = await User.findOne({
+        username: req.body.username,
+      });
 
-            if(user){
-                user.avatar = 'avatar.png';
-                await user.save();
-                res.status(HttpStatus.Ok).json({message: 'Avatar removed successfully!'});
-            } 
+      if (user) {
+        user.avatar = req.file.filename;
+        await user.save();
+        res
+          .status(HttpStatus.Ok)
+          .json({ message: "Avatar uploaded successfully!" });
+      }
+    } catch (error) {
+      res.status(HttpStatus.ServerError).json({ message: error.message });
+    }
+  },
 
-        } catch (error){
-            res.status(HttpStatus.ServerError).json({message: error.message});
-        }
-    },
+  removeAvatar: async (req, res) => {
+    try {
+      const user = await User.findOne({
+        username: req.body.username,
+      });
 
-    updatePoints: async (req, res) => {
-        try{
-            const user = await User.findOne({
-                username: req.body.username
-            });
+      if (user) {
+        user.avatar = "avatar.png";
+        await user.save();
+        res
+          .status(HttpStatus.Ok)
+          .json({ message: "Avatar removed successfully!" });
+      }
+    } catch (error) {
+      res.status(HttpStatus.ServerError).json({ message: error.message });
+    }
+  },
 
-            if(user){
-                user.points += req.body.earnedPoints;
-                await user.save();
-                res.status(HttpStatus.Ok).json({message: 'Points updated successfully!'});
-            } 
+  updatePoints: async (req, res) => {
+    try {
+      const user = await User.findOne({
+        username: req.body.username,
+      });
 
-        } catch (error){
-            res.status(HttpStatus.ServerError).json({message: error.message});
-        }
-    },
-}
+      if (user) {
+        user.points += req.body.earnedPoints;
+        await user.save();
+        res
+          .status(HttpStatus.Ok)
+          .json({ message: "Points updated successfully!" });
+      }
+    } catch (error) {
+      res.status(HttpStatus.ServerError).json({ message: error.message });
+    }
+  },
+};
 
 export default UserController;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,65 +1,72 @@
-import express from 'express';
-import UserController from '../controllers/userController.js';
-import userValidator from '../validators/userValidator.js';
-import validate from '../validators/validator.js';
-import path from 'path';
-import multer from 'multer';
+import express from "express";
+import UserController from "../controllers/userController.js";
+import userValidator from "../validators/userValidator.js";
+import validate from "../validators/validator.js";
+import path from "path";
+import multer from "multer";
 
 const app = express();
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'avatar/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.fieldname + path.extname(file.originalname));
-    }
+  destination: function (req, file, cb) {
+    cb(null, "avatar/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      Date.now() + "-" + file.fieldname + path.extname(file.originalname)
+    );
+  },
 });
 
 const imageFilter = function (req, file, cb) {
-    if(!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG)$/)) {
-        req.fileValidationError = 'Only images allowed';
-        return cb(new Error('Only images allowed!'), false);
-    }
-    cb(null, true);
-}
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG)$/)) {
+    req.fileValidationError = "Only images allowed";
+    return cb(new Error("Only images allowed!"), false);
+  }
+  cb(null, true);
+};
 
 const upload = multer({
-    storage: storage,
-    fileFilter: imageFilter,
-    limits: {fileSize: 3000000}
+  storage: storage,
+  fileFilter: imageFilter,
+  limits: { fileSize: 3000000 },
 });
 
-app.post('/upload', upload.single("avatar"), async (req, res) => {
-    await UserController.upload(req, res);
-})
+app.post("/upload", upload.single("avatar"), async (req, res) => {
+  await UserController.upload(req, res);
+});
 
-app.post('/removeAvatar', async (req, res) => {
-    await UserController.removeAvatar(req, res);
-})
+app.post("/removeAvatar", async (req, res) => {
+  await UserController.removeAvatar(req, res);
+});
 
-app.post('/update', async (req, res) => {
-    await UserController.updatePoints(req, res);
-})
+app.post("/update", async (req, res) => {
+  await UserController.updatePoints(req, res);
+});
 
-app.get('/', async(req, res) => {
-    await UserController.getAll(req, res);
-})
+app.get("/", async (req, res) => {
+  await UserController.getAll(req, res);
+});
 
-app.get('/usersbypoints', async(req, res) => {
-    await UserController.getAllByPoints(req, res);
-})
+app.get("/usersbypoints", async (req, res) => {
+  await UserController.getAllByPoints(req, res);
+});
 
-app.post('/',  userValidator.registerRequest(), validate, async(req, res) => {
-    UserController.create(req, res);
-})
+app.post("/", userValidator.registerRequest(), validate, async (req, res) => {
+  UserController.create(req, res);
+});
 
-app.post('/login',  userValidator.loginRequest(), validate, async(req, res) => {
-    await UserController.login(req, res);
-})
+app.post("/login", userValidator.loginRequest(), validate, async (req, res) => {
+  await UserController.login(req, res);
+});
 
-app.post('/username', async(req, res) => {
-    await UserController.getUserByUsername(req, res);
-})
+app.post("/username", async (req, res) => {
+  await UserController.getUserByUsername(req, res);
+});
+
+app.post("/resetpassword", async (req, res) => {
+  await UserController.resetPassword(req, res);
+});
 
 export default app;
