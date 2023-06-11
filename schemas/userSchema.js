f4aa590a850521e69cdccbbdb76c5d5f4b5e0b84
David <davidmarian2012@gmail.com>
2023-06-12 00:46:51 +0300

    mail confirmation

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,45 +1,52 @@
 import mongoose from "mongoose";
-import bcrypt from 'mongoose-bcrypt';
+import bcrypt from "mongoose-bcrypt";
 
-const userSchema = mongoose.Schema({
-        _id: mongoose.Schema.Types.ObjectId,
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            minLength: 3,
-            maxLength: 25
-        },
-        password: {
-            type: String,
-            required: true,
-            minLength: 3
-        },
-        points: {
-            type: Number
-        },
-        avatar: {
-            type: String,
-            default: 'avatar.png'
-        }
+const userSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-        toJSON: {virtuals: true},
-        toObject: {virtuals: false},
-        timestamps: true
-    }
-)
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 3,
+      maxLength: 25,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 3,
+    },
+    points: {
+      type: Number,
+    },
+    avatar: {
+      type: String,
+      default: "avatar.png",
+    },
+    activationToken: {
+      type: String,
+    },
+    isActive: {
+      type: Boolean,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: false },
+    timestamps: true,
+  }
+);
 
 userSchema.plugin(bcrypt, {
-    fields: ['password'],
-    rounds: 10
+  fields: ["password"],
+  rounds: 10,
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
